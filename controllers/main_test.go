6adfc51602953e605fd52a6fecedfd5864126579
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-11-19 13:22:58 +0900

    up to ch.08 implement http handler using service mock or gorilla/mux

--- controllers/main_test.go
+++ controllers/main_test.go
@@ -1,32 +1,17 @@
 package controllers_test
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
-	"os"
 	"testing"
 
 	"github.com/MatsuoTakuro/myapi-go-intermediate/controllers"
-	"github.com/MatsuoTakuro/myapi-go-intermediate/services"
+	"github.com/MatsuoTakuro/myapi-go-intermediate/controllers/testdata"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 var aCon *controllers.ArticleController
 
 func TestMain(m *testing.M) {
-	dbUser := "docker"
-	dbPassword := "docker"
-	dbDatabase := "sampledb"
-	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
-
-	db, err := sql.Open("mysql", dbConn)
-	if err != nil {
-		log.Println("DB setup fail")
-		os.Exit(1)
-	}
-
-	ser := services.NewMyAppService(db)
+	ser := testdata.NewServiceMock()
 	aCon = controllers.NewArticleController(ser)
 
 	m.Run()
--- go.mod
+++ go.mod
@@ -6,3 +6,11 @@ require (
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gorilla/mux v1.8.0
 )
+
+require (
+	github.com/josharian/impl v1.1.0 // indirect
+	golang.org/x/mod v0.7.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/tools v0.3.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+)
